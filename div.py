c248c800071fd699d70dc5e5a51f7325d3a31500
Samuvel Rajappa <samuvelrajappa@gmail.com>
2026-06-30 15:04:57 +0530

    div,mul,sub_modified

--- div.py
+++ div.py
@@ -1,8 +1,11 @@
 print("This is a program to divide two numbers in a loop and store the quotient in a list.")
 n=int(input("Enter the Length of loop: "))
-sum=[]
+quotient=[]
+remainder=[]
 for i in range(n):
-    variable1=int(input("Enter the " + str(i+1) + " Loop First Number: "))
-    variable2=int(input("Enter the " + str(i+1) + " Loop Second Number: "))
-    sum.append(variable1/variable2)
-print("Show the List:", sum)
+    variable1=int(input("Enter the " + str(i+1) + " Loop Dividend Number: "))
+    variable2=int(input("Enter the " + str(i+1) + " Loop Divisor Number: "))
+    quotient.append(variable1/variable2)
+    remainder.append(variable1%variable2)
+print("Show the quotients:", quotient)
+print("Show the remainders:", remainder)
--- mul.py
+++ mul.py
@@ -1,8 +1,8 @@
 print("This is a program to multiply two numbers in a loop and store the product in a list.")
 n=int(input("Enter the Length of loop: "))
-sum=[]
+mul=[]
 for i in range(n):
     variable1=int(input("Enter the " + str(i+1) + " Loop First Number: "))
     variable2=int(input("Enter the " + str(i+1) + " Loop Second Number: "))
-    sum.append(variable1*variable2)
-print("Show the List:", sum)
+    mul.append(variable1*variable2)
+print("Show the List:", mul)
