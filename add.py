1e3b630a63de55ea31b9f4a7e9e4fe91384bdb7a
Samuvel Rajappa <samuvelrajappa@gmail.com>
2026-06-30 11:30:23 +0530

    main_add.py_file_modified

---
+++ add.py
@@ -0,0 +1,7 @@
+n=int(input("Enter the Length of loop"))
+sum=[]
+for i in range(n):
+    variable1=int(input("Enter the First number"))
+    variable2=int(input("Enter the Second nunber"))
+    sum=variable1+variable2
+print("Total Elements", sum)
