904b9039b30c595583f4b17c3ae0af404fe18e7e
Samuvel Rajappa <106525310+samuvelrajappa@users.noreply.github.com>
2026-06-30 16:20:49 +0530

    Delete mul.py

--- mul.py
+++
@@ -1,8 +0,0 @@
-print("This is a program to multiply two numbers in a loop and store the product in a list.")
-n=int(input("Enter the Length of loop: "))
-mul=[]
-for i in range(n):
-    variable1=int(input("Enter the " + str(i+1) + " Loop First Number: "))
-    variable2=int(input("Enter the " + str(i+1) + " Loop Second Number: "))
-    mul.append(variable1*variable2)
-print("Show the List:", mul)
